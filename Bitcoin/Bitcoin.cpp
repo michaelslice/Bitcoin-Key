a24eb1bae54b7d2f7e2512cc8036cc43198199d1
Michael Slice <mjsens523@gmail.com>
2023-07-16 22:25:09 -0500

    Added Base58 to Decimal Conversion

--- Bitcoin/Bitcoin.cpp
+++ Bitcoin/Bitcoin.cpp
@@ -1,28 +1,43 @@
-// Bitcoin.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include "Decrypt.h"
+
+
+
+char convertBase58toDecimal(char x)
+{
+    // base 58 array
+    const std::string base58array = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    int index = base58array.find(x);
+
+    std::cout << index;
+
+    // error handling 
+    if (index == std::string::npos)
+    {
+        std::cout << "Invalid Base58 Character" << std::endl;
+        return -1;
+    }
+    return index;
+
+
+};
 
 int main()
 {
     // bitcoin wallet 
     std::string public_key{ "1E1eZNRxbNHQNPEraaCZw1YvuXhDCbtEid "};
-    
     std::string private_key{"L5buqNXpTnu7eAUusKnHdW74gyjczV6B77hmTE1DUqnpkVv61ste"};
 
-    std::cout << sizeof(private_key) << std::endl;
-    
-    
-    std::cout << "Hello World!\n";
+    // convert bae 58 to decimal
+    char base58char = '3';
+    int decimalValue = convertBase58toDecimal(base58char);
+    if (decimalValue != -1) {
+        std::cout << "Decimal value of '" << base58char << "' in Base58: " << decimalValue << std::endl;
+    }
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Bitcoin/Decrypt.h
@@ -0,0 +1,7 @@
+#pragma once
+class Decrypt
+{
+public:
+	char convertBase58toDecimal;
+};
+
