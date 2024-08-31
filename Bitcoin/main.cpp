0d1b0ce03ff092630d1f980bbec90ec1fb3a6265
michaelslice <mjsens523@gmail.com>
2024-08-31 14:40:27 -0500

    Added key calculation

--- Bitcoin/main.cpp
+++ Bitcoin/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <math.h>
 #include <tuple>
 #include <random>
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <boost/multiprecision/cpp_int.hpp>
+
 using namespace std;
+using namespace boost::multiprecision;
+
+using bigint = cpp_int;
 
 /**
  *  Perform modular exponentiation
  */
-int mod_pow(int x, int y, int p){
-    int result = 1;
+bigint mod_pow(bigint x, bigint y, bigint p){
+    bigint result = 1;
     x = x % p;
 
     while(y > 0){
@@ -19,7 +23,7 @@ int mod_pow(int x, int y, int p){
             result = (result * x) % p;
         }
 
-        y = x >> 1;
+        y >>= 1;
         x = (x * x) % p;
     }
     return result;
@@ -29,16 +33,16 @@ int mod_pow(int x, int y, int p){
  * Find the modular inverse of a under modulo p, p must be a prime number
  * 
  */
-int mod_inv(int a,int p){
+bigint mod_inv(bigint a, bigint p){
     return mod_pow(a, p - 2, p);
 }
 
 /**
  * Add two points on an elliptic curve
  */
-tuple<int, int> elliptic_curve_add(tuple<int, int> p1, tuple<int, int> p2, int a, int p){
+tuple<bigint, bigint> elliptic_curve_add(tuple<bigint, bigint> p1, tuple<bigint, bigint> p2, bigint a, bigint p){
     
-    int x1, y1, x2, y2;
+    bigint x1, y1, x2, y2;
     tie(x1, y1) = p1;
     tie(x2, y2) = p2;
 
@@ -46,7 +50,7 @@ tuple<int, int> elliptic_curve_add(tuple<int, int> p1, tuple<int, int> p2, int a
     if (x1 == 0 && y1 == 0) return p2;
     if (x2 == 0 && y2 == 0) return p1;
 
-    int m = 0;
+    bigint m = 0;
     if (x1 == x2 && y1 == y2) {
         // Use tangent formula 
         m = (3 * x1 * x1 + a) * mod_inv(2 * y1, p) % p;
@@ -55,8 +59,8 @@ tuple<int, int> elliptic_curve_add(tuple<int, int> p1, tuple<int, int> p2, int a
         m = (y2 - y1) * mod_inv(x2 - x1, p) % p;
     }
 
-    int x3 = (m * m - x1 - x2) % p;
-    int y3 = (m * (x1 - x3) - y1) % p;
+    bigint x3 = (m * m - x1 - x2) % p;
+    bigint y3 = (m * (x1 - x3) - y1) % p;
 
     if (x3 < 0) x3 += p;
     if (y3 < 0) y3 += p;
@@ -67,9 +71,9 @@ tuple<int, int> elliptic_curve_add(tuple<int, int> p1, tuple<int, int> p2, int a
 /**
  * Multiply a point by a scalar on an elliptic curve
  */
-tuple<int, int> scalar_multiply(int k, tuple<int, int> point, int a, int p) {
-    tuple<int, int> result = make_tuple(0, 0);
-    tuple<int, int> addend = point;
+tuple<bigint, bigint> scalar_multiply(bigint k, tuple<bigint, bigint> point, bigint a, bigint p) {
+    tuple<bigint, bigint> result = make_tuple(0, 0);
+    tuple<bigint, bigint> addend = point;
 
     while (k > 0) {
         if (k & 1) {
@@ -91,21 +95,27 @@ int main(){
 
     stringstream ss;
     ss << hex << setw(16) << setfill('0') << random_number; 
-    cout << ss.str() << '\n'; 
+    string private_key_str = ss.str(); 
 
-    string private_key = static_cast<int>("%064x" % random_number);
+    bigint private_key = bigint("0x" + private_key_str);
 
-    string G_x = "0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F817";
-    string G_y = "0x483ADA7726A3C4655DA4FBFC0E1108A8FD17D448A68554199C47D08F4E7D3";
-    auto G = (G_x, G_y);
-    string p = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F";
-    int a = 0;
-    int b = 7; 
+    bigint G_x("0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F817");
+    bigint G_y("0x483ADA7726A3C4655DA4FBFC0E1108A8FD17D448A68554199C47D08F4E7D3");
+    auto G = make_tuple(G_x, G_y);
+    bigint p("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F");
+    bigint a = 0;
+    bigint b = 7; 
 
-    string public_key = scalar_multiply(private_key, G, a, p);
+    auto public_key = scalar_multiply(private_key, G, a, p);
+    bigint x = get<0>(public_key);
+    bigint y = get<1>(public_key);
 
-    cout << "Your Public Key is " << public_key << '\n';
-    cout << "Your Private Key is " << private_key << '\n';
+    string prefix = (y % 2 == 0) ? "02" : "03";
+    ss << hex << setw(64) << setfill('0') << x;
+    string public_key_str = prefix + ss.str(); 
+
+    cout << "Your Public Key is " << public_key_str << '\n';
+    cout << "Your Private Key is " << private_key_str << '\n';
     
     return 0;
 }
